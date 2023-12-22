b22703fbf2f86626f05b3dd9d36604536b524ebf
HeronErin <HeronErin@protonmail.com>
2023-12-22 01:27:12 -0500

    Fix python ver

--- script.hello.world/addon.py
+++ script.hello.world/addon.py
@@ -1,11 +1,12 @@
 import xbmcaddon
 import xbmcgui
- 
+import sys
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
+URL = sys.argv[0]
 
 # Set a string variable to use 
-line1 = "Hello World! We can write anything we want here Using Python"
+line1 = "Hello World! " + URL
 
 # Launch a dialog box in kodi showing the string variable 'line1' as the contents
 xbmcgui.Dialog().ok(addonname, line1)
